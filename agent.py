c1f539db7bf3f2842cf4b069c9e09f73a060372e
Anna-Sofia Paavonen <anna.paavonen@gmail.com>
2026-06-09 13:16:03 +0300

    removed unnecessary code and comments

--- agent.py
+++ agent.py
@@ -12,36 +12,12 @@ class Message:
     payload: dict
 
 
-# ==================================================================================
-# Local messaging (ZeroMQ) server
-# ==================================================================================
-
-async def start(node):
-    while True:
-        msg = await node.bus.get_local_message()
-        if msg == None:
-            print(f"{TAG} No bid received from local peer.")
-            continue
-        task_id = msg.payload.get("task_id")
-        task_type = msg.payload.get("task_type")
-
-        if task_type == "task_request":
-            print(f"\n{TAG} Received task request: task id={task_id}, type={task_type}")
-            await node.bus.local_request({"msg": "ack"})
-
-
-# ===================================================================================
-# NATS message handlers
-# ===================================================================================
-
-
 def register(node):
     """Register agent's message handlers to the node's MessageBus."""
     node.bus.on("task_request", handle_task_request)
     node.bus.on("task_assignment", handle_task_assignment)
 
 
-# TODO: add also the messagebus as a parameter here, for sending bids
 def handle_task_request(task_req: dict) -> dict:
     task_id = task_req.get("task_id")
     task_type = task_req.get("task_type")
@@ -50,7 +26,6 @@ def handle_task_request(task_req: dict) -> dict:
           "     task id={task_id}" +
           "     type={task_type}\n")
 
-    # TODO: send back also originator_node id and originator_lan in the reply
     return {"msg": "ack"}
 
 
