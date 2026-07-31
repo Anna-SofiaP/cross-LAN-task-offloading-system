96b9ba016e3f3c5a5d74e66a1039b30a808a6c6d
Anna-Sofia Paavonen <anna.paavonen@gmail.com>
2026-07-31 15:12:02 +0300

    test: try to fix async function call from sync function

--- agent.py
+++ agent.py
@@ -115,7 +115,7 @@ def execute_task(node, task_id: str, orig_peer_id: str):
 
     # TODO: fixt the task result sending to the originator node!
     #NOTE: Old version: response = asyncio.run(node.bus.global_request((peer_info[0], peer_info[1], peer_info[2]), task_result))
-    response = asyncio.run(node.bus.request((peer_info[0], peer_info[1], peer_info[2]), task_result))
+    response = asyncio.get_event_loop().run_until_complete(node.bus.request((peer_info[0], peer_info[1], peer_info[2]), task_result))
 
     print(f"{TAG} Task result for {task_id}: {task_result.payload['result']}")
     if response.type == "ack":
