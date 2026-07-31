2f3f4172295282f58f07640766076cd439ec8f4c
Anna-Sofia Paavonen <anna.paavonen@gmail.com>
2026-07-31 15:20:44 +0300

    sending task result to originator: second attempt

--- agent.py
+++ agent.py
@@ -85,6 +85,10 @@ def record_task(node, task_id: str):
     node.task_cache.append({"success": 1})
 
 
+async def send_task_result(node, peer_info: tuple, task_result: Message):
+    return await node.bus.request((peer_info[0], peer_info[1], peer_info[2]), task_result)
+
+
 def execute_task(node, task_id: str, orig_peer_id: str):
     # Simulate task execution with a sleep
     print(f"{TAG} Executing {task_id} ...")
@@ -115,7 +119,7 @@ def execute_task(node, task_id: str, orig_peer_id: str):
 
     # TODO: fixt the task result sending to the originator node!
     #NOTE: Old version: response = asyncio.run(node.bus.global_request((peer_info[0], peer_info[1], peer_info[2]), task_result))
-    response = asyncio.get_event_loop().run_until_complete(node.bus.request((peer_info[0], peer_info[1], peer_info[2]), task_result))
+    response = asyncio.run(send_task_result(node, peer_info, task_result))
 
     print(f"{TAG} Task result for {task_id}: {task_result.payload['result']}")
     if response.type == "ack":
