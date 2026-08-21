b8e54afffe744fd67cc6f180abc8ed8fbfe1297e
Anna-Sofia Paavonen <anna.paavonen@gmail.com>
2026-08-21 14:57:19 +0300

    modified: removed old score calculation code and bid ranking, added new bid ranking logic and offloading decision making logic

--- task_originator.py
+++ task_originator.py
@@ -4,8 +4,8 @@ import itertools
 import random
 from time import time
 import uuid
-from lstm_scoring import load_balanced_score
 from task_assign import assign_task, enqueue_retry, record_assignment
+from offloading_decision_making import load_balanced_score, reliability_and_privacy_assessment, make_final_offloading_decision
 #from logger import log_latency
 
 TASK_TYPES              = ["CLASSIFICATION", "TIMESERIES", "CV_INFERENCE"]
@@ -176,24 +176,46 @@ async def run_negotiation(node, task_id: str, task_type: str, in_data_privacy_lv
 
     # Pass all bidding node id:s so load_balanced_score sees the full picture
     all_bidders = [bid["node_id"] for bid in bids]
-    ranked = sorted(bids,
-        key=lambda bid: load_balanced_score(node, bid, all_bidders), reverse=True)
+
+    # NOTE: descending order: bigger values are better
+    # TODO: this doesn't need to be sorted! The values just need to be calculated!
+    #score_ranked = sorted(bids,
+    #    key=lambda bid: load_balanced_score(node, bid, all_bidders), reverse=True)
+
+    # TODO: move this to the bottom!
+    #for i, bid in enumerate(score_ranked):
+    #    peer_id = bid["node_id"]
+    #    print(f"{i+1}. {peer_id}: raw score={bid["score"]}, adjusted score={bid["adj_score"]}, risk={bid["risk"]}\n"
+    #          f"    reliability level={bid["reliability_lvl"]}, privacy level={bid["privacy_lvl"]}")
+
+    ## TODO: This doesn't need to be sorted! The values just need to be calculated!
+    #pr_ranked = sorted(score_ranked,
+    #    key=lambda bid: reliability_and_privacy_assessment(bid, in_data_privacy_lvl, out_data_privacy_lvl, task_priority))
+
+    for bid in bids:
+        bid["adj_score"] = load_balanced_score(node, bid, all_bidders)
+        bid["pr_score"] = reliability_and_privacy_assessment(bid, in_data_privacy_lvl, out_data_privacy_lvl, task_priority)
+
+    final_ranking = sorted(bids,
+        key=lambda bid: make_final_offloading_decision(bid, bids), reverse=True)
     
     print(f"\n{TAG} Final ranking: \n")
-    for i, bid in enumerate(ranked):
+
+    for i, bid in enumerate(final_ranking):
         peer_id = bid["node_id"]
-        print(f"{i+1}. {peer_id}: raw score = {bid["score"]}, adjusted score = {bid["adj_score"]}, risk = {bid["risk"]}")
+        print(f"{i+1}. {peer_id}: raw score={bid["score"]}, adj. score={bid["adj_score"]}, "
+              f"pr score={bid["pr_score"]}, final score (WP)={bid["w_product"]}")
 
     results                   = {}
     results["task_id"]        = task_id
     results["task_type"]      = task_type
-    results["all_bids"]       = ranked
+    results["all_bids"]       = final_ranking
     results["task_req_start"] = task_req_start
     results["last_bid_time"]  = last_bid_time
 
     #print(f"{TAG} Selected winner: {ranked[0]["node_id"]}")
 
-    print(f"{TAG} Ranking: {results}")
+    #print(f"{TAG} Ranking: {results}")
 
     return results
 
